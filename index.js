c8e91f014d67befae4ca8d7a35ecc052766d56e2
Logan Talkington <talkington.logan@gmail.com>
2021-06-17 21:23:02 -0500

    updating with js

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 "use strict";
+
+let currentColor = "black";
+let rainbowButtonElem = document.getElementById("rainbow");
+let blackButtonElem = document.getElementById("black");
+let whiteButtonElem = document.getElementById("white");
+let randomButtonElem = document.getElementById("random");
+let boxesElem = document.getElementById("box1");
+
+function rainbowClick() {
+  currentColor = "rainbow";
+  console.log(currentColor);
+}
+
+function blackClick() {
+  currentColor = "black";
+  console.log(currentColor);
+}
+
+function whiteClick() {
+  currentColor = "white";
+  console.log(currentColor);
+}
+
+function randomClick() {
+  currentColor = "random";
+  console.log(currentColor);
+}
+
+function colorBox() {
+  boxesElem.style.backgroundColor = currentColor;
+  console.log(currentColor);
+}
+
+rainbowButtonElem.addEventListener("click", rainbowClick);
+blackButtonElem.addEventListener("click", blackClick);
+whiteButtonElem.addEventListener("click", whiteClick);
+randomButtonElem.addEventListener("click", randomClick);
+boxesElem.addEventListener("click", colorBox);
