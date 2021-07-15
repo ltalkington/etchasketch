4ad7b012034adf3e4c98d71e539e715ecb99d680
Logan Talkington <talkington.logan@gmail.com>
2021-07-14 22:15:23 -0500

    committing new updates to grid

--- index.js
+++ index.js
@@ -1,39 +1,49 @@
 "use strict";
 
-let currentColor = "black";
-let rainbowButtonElem = document.getElementById("rainbow");
-let blackButtonElem = document.getElementById("black");
-let whiteButtonElem = document.getElementById("white");
-let randomButtonElem = document.getElementById("random");
-let boxesElem = document.getElementById("box1");
+let boxElem = document.getElementById("boxes-div");
+let box = document.querySelector(".boxContainer");
+let newInput = document.getElementById("rows");
+let resetButton = document.querySelector(".reset");
+let newBlock = document.querySelector(".right");
 
-function rainbowClick() {
-  currentColor = "rainbow";
-  console.log(currentColor);
-}
+let createGrid = () => {
+  // This will create a grid to use up to 256 which is a 16 by 16 grid
+  for (let i = 0; i < 256; i++) {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    box.appendChild(div);
+  }
+};
 
-function blackClick() {
-  currentColor = "black";
-  console.log(currentColor);
-}
+let updateGrid = () => {
+  box.innerHTML = "";
+  box.style.setProperty(
+    "grid-template-columns",
+    `repeat(${newInput.value}, 2fr)`
+  );
+  box.style.setProperty("grid-template-rows", `repeat(${newInput.value}, 2fr)`);
+  for (let i = 0; i < newInput.value * newInput.value; i++) {
+    // This will adjust the rows and columns of the program based on the number the user put in
+    const div = document.createElement("div");
+    div.classList.add("square");
+    box.appendChild(div);
+  }
+  console.log(newInput.value);
+};
 
-function whiteClick() {
-  currentColor = "white";
-  console.log(currentColor);
-}
+console.log(newBlock);
+newBlock.addEventListener("mouseover", function (event) {
+  event.target.classList.replace("square", "color");
+});
 
-function randomClick() {
-  currentColor = "random";
-  console.log(currentColor);
-}
+newInput.addEventListener("change", updateGrid);
 
-function colorBox() {
-  boxesElem.style.backgroundColor = currentColor;
-  console.log(currentColor);
-}
+resetButton.addEventListener("click", function () {
+  box.innerHTML = "";
+  newInput.value = "";
+  box.style.setProperty("grid-template-columns", `repeat(16, 2fr)`);
+  box.style.setProperty("grid-template-rows", `repeat(16, 2fr)`);
+  createGrid();
+});
 
-rainbowButtonElem.addEventListener("click", rainbowClick);
-blackButtonElem.addEventListener("click", blackClick);
-whiteButtonElem.addEventListener("click", whiteClick);
-randomButtonElem.addEventListener("click", randomClick);
-boxesElem.addEventListener("click", colorBox);
+createGrid();
